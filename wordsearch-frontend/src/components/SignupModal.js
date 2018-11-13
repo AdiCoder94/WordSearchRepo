6517322db18a82f5ea3aec312f10e63fe703e9e9
Aditya Patel <aditya.p.intern@aspireinfolabs.com>
2018-11-13 18:57:01 +0530

    made changes in the sign up form

--- wordsearch-frontend/src/components/SignupModal.js
+++ wordsearch-frontend/src/components/SignupModal.js
@@ -8,36 +8,43 @@ class SignupModal extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			viewLoginModal:true
+			viewSignUpModal:true
 		}
 		this.hideModal = this.hideModal.bind(this);
 	}
 	hideModal(){
 		this.setState(state =>({
-				viewLoginModal:false
+				viewSignUpModal:false
 			})
 		)
-		console.log(this.state.viewLoginModal + "from child");
 	}
 
 	render(props){
 		let hideModalClassName;
-		(this.state.viewLoginModal) ? (hideModalClassName = "modal-container") : (hideModalClassName="hidden")
+		(this.state.viewSignUpModal) ? (hideModalClassName = "modal-container") : (hideModalClassName="hidden")
 		return(
 			<React.Fragment>
 				<div className={hideModalClassName}>
-					<div className="modal-content flex-column">
+					<div className="signup-modal-content flex-column">
 						<div className="username-section flex-row">	
 							<p className="username-subheading">User name:</p>
+							<input type='text' />
+						</div>
+						<div className="email-section flex-row">	
+							<p className="email-subheading">Email:</p>
 							<input type='text' />
 						</div>
 						<div className="password-section flex-row">
 							<p className="password-subheading">Password:</p>
 							<input type='text' />
 						</div>
+						<div className="confirm-password-section flex-row">
+							<p className="confirm-password-subheading">Confirm password:</p>
+							<input type='text' />
+						</div>
 						<div className='login-cancel-holder flex-row'>	
 							<button className='close-modal-btn modal-btn' onClick={this.hideModal}>Cancel</button>
-							<button className="submit-form-btn modal-btn">Login</button>
+							<button className="submit-form-btn modal-btn">Submit</button>
 						</div>
 					</div>
 				</div>
--- wordsearch-frontend/src/components/header.js
+++ wordsearch-frontend/src/components/header.js
@@ -30,23 +30,26 @@ class Header extends Component{
 				viewSignupModal:!this.state.viewSignupModal
 			})
 		)		
-		console.log(this.state.viewLoginModal);
+		console.log(this.state.viewSignupModal +"signup");
 	}
 
 	render(){
 		let LogInModal;
-		let SignUpModal;
+		let SignupModal;
 		this.state.viewLoginModal ? (LogInModal = <LoginModal />) : (LogInModal = <React.Fragment />);
-		this.state.viewSignupModal ? (SignUpModal = <LoginModal />) : (SignUpModal = <React.Fragment />);
+		this.state.viewSignupModal ? (SignupModal = <SignUpModal />) : (SignupModal = <React.Fragment />);
   		return(
 		    <React.Fragment>
 		      <div className="site-header flex-row">
 		        <h2 className='site-name'>Word search</h2>
-		        <button className='signup-btn' onClick={this.showSignupModal}>Sign up</button>	        
-		        <button className='login-btn' onClick={this.showLoginModal}>Login</button>	        
+		        <div className="btn-holder">
+			        <button className='signup-btn' onClick={this.showSignupModal}>Sign up</button>	        
+			        <button className='login-btn' onClick={this.showLoginModal}>Login</button>
+	      	  
+		        </div>	        
 		      </div>
+		      {SignupModal}
 	      	  {LogInModal}
-	      	  {SignUpModal}
 		    </React.Fragment>
     	)
    }
