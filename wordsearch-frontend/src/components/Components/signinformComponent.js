8d6448a946154b1b5c431ae0182771963f180be4
AdityaCoder <acceptthegiftap@gmail.com>
2021-02-03 09:56:37 +0530

    Fixed landing page

--- wordsearch-frontend/src/components/Components/signinformComponent.js
+++ wordsearch-frontend/src/components/Components/signinformComponent.js
@@ -9,19 +9,13 @@ import '../../scss/article_styles.scss';
 import Spinner from "../Components/spinner";
 
 function SignInLoadingComponent(){
-	console.log('loading')
 	return(
 		<div className='loader-container'>
-			{/* <Spinner /> */}
-			<p className='pleasewait-txt'>Please wait...</p>
-			
-			</div>)
-}
+			<Spinner /></div> )}
 
 function ErrMessageComponent(props){
 	return(
-		<p className='message-container'>{props.msg}</p>)
-}
+		<p className='message-container'>{props.msg}</p> )}
 
 class SigninFormComponent extends Component{
 	constructor(){
@@ -47,20 +41,15 @@ class SigninFormComponent extends Component{
 			window.sessionStorage.setItem('token', this.props.signinState.token)
 			window.location.href = `${frontendURL}${memberDashboardURL}`
 		}
-		if(this.props.signinState.isFetching){
-			console.log('hello')
-		}
 	}		
 
 	render(){
-		let hideModalClassName, statusDisplayComponent;
+		let hideModalClassName, statusDisplayComponent, loadingDisplayComponent;
+		
 		(this.state.viewLoginModal) ? (hideModalClassName = "modal-container") : (hideModalClassName="hidden");
-		// (this.props.signinState.isFetching) ? (loaderClassName = 'loader-container flex-row') : (loaderClassName = 'hidden');
-		// (this.props.signinState.isErr) ? (messageClassName='message-container') : (messageClassName = 'hidden');
-
-		(this.props.signinState.isFetching) ? (statusDisplayComponent = <SignInLoadingComponent />) : (statusDisplayComponent = <React.Fragment />);
+		(this.props.signinState.isFetching) ? (loadingDisplayComponent = <SignInLoadingComponent />) : (loadingDisplayComponent = <React.Fragment />);
 		(this.props.signinState.isErr) ? (statusDisplayComponent = <ErrMessageComponent msg={this.props.signinState.err} />) : (statusDisplayComponent = <React.Fragment />);
-	
+
 		var { signUpEmail, logInPassword } = this.state
 		const userCred = { signUpEmail, logInPassword }
 		
@@ -73,27 +62,11 @@ class SigninFormComponent extends Component{
 						<div className="password-section flex-row">
 							<p className="password-subheading">Password:</p>
 							<input type='password' value={logInPassword} onChange={this.onTextboxChangeLogInPassword} /></div>
-						{/* <div className='signin-holder'>	
-							<div className='requeststatus-display flex-column'>
-								<p className={messageClassName}>{this.props.signinState.err}</p>
-								<div className={loaderClassName}>
-									<Spinner />
-								<p className='pleasewait-txt'>Please wait...</p></div>
-							</div>
- 							<button className="signin-btn modal-btn" onClick={() => this.props.onSignIn(userCred)}>Sign In</button>
-
-						
-
-
-
-						 </div> */}
-						 <div className='signin-holder flex-row'>
-								{statusDisplayComponent}
-						 		<button className="signin-btn modal-btn" onClick={() => this.props.onSignIn(userCred)}>Sign In</button>
-						 </div>
-						 
-						 
-						 </div></React.Fragment>)}}
+						<div className='signin-holder flex-row'>
+							{ statusDisplayComponent }
+							{ loadingDisplayComponent }
+							<button className="signin-btn modal-btn" onClick={() => this.props.onSignIn(userCred)}>Sign In</button>
+						</div></div></React.Fragment>)}}
 
 const mapStateToProps = (state) => {
 	return {
